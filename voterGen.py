c6f3dbbb2d9d6dd93920dd8aa85ea50bbbea64d1
Athul Anoop <athulanoop2020@gmail.com>
2022-09-12 18:05:30 +0530

    Improved sample data generator script

--- voterGen.py
+++ voterGen.py
@@ -1,48 +1,25 @@
-#import pickle,csv, names, uuid, random
+import pickle,csv, names, uuid, random
 import csv
 from helper import *
-# sex = ["M", "F", "Other"]
 
-# def genVoter(n, uAge=18, lAge=100, ):
+sex = ["M", "F", "Other"]
 
-#     voters = []
-#     for i in range(n):
-#         vID = str(uuid.uuid4()).split("-")[0]
-#         vName = names.get_full_name()
-#         vAge = random.randint(uAge,lAge)
-#         vSex = random.choice(sex)
-#         voters.append([vID,vName,vAge,vSex])
-#     return voters
+def genVoter(n, lAge=18, uAge=100, ):
 
-# def dep(voters):
-#     with open("voterList.csv", "w") as f:
-#         wO = csv.writer(f)
-#         wO.writerow(["Name", "Age", "Sex"])
-#         for i in voters:
-#             wO.writerow(i[1:])
+    voters = []
+    for i in range(n):
+        vID = str(uuid.uuid4()).split("-")[0]
+        vName = names.get_full_name()
+        vAge = random.randint(lAge,uAge)
+        vSex = random.choice(sex)
+        voters.append([vID,vName,vAge,vSex,"N"])
+    return voters
 
-#     with open("voterList.dat", 'wb') as f:
-#         for i in voters:
-#             pickle.dump({"ID":i[0], "Name":i[1]}, f)
+def dep(voters):
+    with open("voterList.csv", "w") as f:
+        wO = csv.writer(f)
+        wO.writerow(["ID", "Name", "Age", "Sex", "Voted"])
+        for i in voters:
+            wO.writerow(i)
 
-# dep(genVoter(50))
-# data = fetchVoters()
-# formattedData = []
-# for i in data:
-#     formattedData.append(i+["N"])                     added Voted attribute
-# with open("Data/VoterList.csv", 'w') as f:
-#     writer = csv.writer(f)
-#     writer.writerows(formattedData)
-
-# data = fetchVoters()
-# data1 = fetchVotersBIN()
-# values = []
-# formattedData = []
-# for i in data1:
-#     values.append(i[0])
-# for i in range(len(data)):
-#     formattedData.append([values[i]]+data[i])
-
-# with open("Data/VoterList.csv", 'w') as f:
-#     writer = csv.writer(f)
-#     writer.writerows(formattedData)
+dep(genVoter(50))
